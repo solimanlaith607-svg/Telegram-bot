974f7ac030063cbdf9c4a8783f5150d7df2a495e
solimanlaith607-svg <solimanlaith607@gmail.com>
2025-09-11 00:51:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
+import os
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+# إعداد اللوجز
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
+# أوامر البوت
 def start(update, context):
     update.message.reply_text("أهلا! البوت شغال ✅")
 
@@ -12,15 +17,21 @@ def echo(update, context):
     update.message.reply_text(update.message.text)
 
 def main():
-    TOKEN ="8223338009:AAEq7NoAattxNKtxRxn0gbR7AS5Ub358meQ"
-    updater = Updater(TOKEN, use_context=True)
+    # جلب التوكن من Environment Variables
+    TOKEN = os.getenv("TOKEN")
 
+    if not TOKEN:
+        logger.error("⚠️ ما لقيت التوكن! لازم تضيف Environment Variable اسمو TOKEN في Render")
+        return
+
+    updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
+
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
     updater.start_polling()
     updater.idle()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
